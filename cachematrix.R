be67f366f7c67145c4d03398cb75c635ef9398c8
raytrace <coursera@yoginaut.com>
2014-04-07 16:40:35 -0400

    solution to problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Create a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invx <- NULL
+  set <- function(y) {
+    x <<- y
+    invx <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) invx <<- inverse
+  getinverse <- function() invx
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix created using makeCacheMatrix().
+## If the inverse has already been calculated and the matrix is unchanged,
+## then return the cached solution.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
+  invx <- x$getinverse()
+  if(!is.null(invx)) {
+    message("getting cached data")
+    return(invx)
+  }
+  data <- x$get()
+  invx <- solve(data, ...)
+  x$setinverse(invx)
+  return(invx)
 }
---
+++ test.R
@@ -0,0 +1,31 @@
+setwd('/Volumes/TheOneThing/Courses/RProgramming/week3/ProgrammingAssignment2')
+
+source('cachematrix.R')
+
+# test basic functionality
+nz <- 1000
+z <- matrix(array(rnorm(nz*nz)),nrow=nz)
+
+x <- makeCacheMatrix(z)
+invz <- cacheSolve(x)
+
+# this should now retrieve the cache
+invz <- cacheSolve(x)
+
+# test if it's the same as solving directly
+invz.raw <- solve(z)
+max(abs(invz.raw-invz))
+
+# try resetting the matrix, which should delete the inverse
+z <- matrix(array(rnorm(nz*nz)),nrow=nz)
+x$set(z)
+
+# retreive the inverse.. this should recalculate it
+invz <- cacheSolve(x)
+
+# see if there is now an error with the raw solve (should be)
+max(abs(invz.raw-invz))
+
+# try resolving and test again
+invz.raw <- solve(z)
+max(abs(invz.raw-invz))
